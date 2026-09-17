ce53c0586cade2a60cf9395e2b0c6188c5f0abce
AngelErickGC <125053125@upq.edu.mx>
2026-09-17 15:33:38 -0600

    Primer Hola Mundo

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- <?php
+echo"Hola mundo!!!"
+?>
+ -->
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color rgb(4, 255, 163)
+        }
+
+    </style>
+ </head>
+ <body>
+    <h1>Hola Cara de bola </h1>
+    <h2>Hola Cara de bola </h2>
+    <h3>Hola Cara de bola </h3>
+    <h4>Hola Cara de bola </h4>
+    <h5>Hola Cara de bola </h5>
+    <h6>Hola Cara de bola </h6>
+
+    
+ </body>
+ </html>
