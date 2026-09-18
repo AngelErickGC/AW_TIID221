e4187ee2f4c24424ac2a1ed0a4ae936fbf7e7f7e
AngelErickGC <125053125@upq.edu.mx>
2026-09-18 17:26:11 -0600

    Diferentes estilos

--- index.php
+++ index.php
@@ -10,8 +10,27 @@ echo"Hola mundo!!!"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        h1{
-            color rgb(4, 255, 163)
+        h1 {
+            color: rgb(4, 255, 163);
+        }
+        
+        .seccion {
+            background-color: #05dbd4;
+            padding: 20px 40px;
+            margin: 20px; 
+            border: 3px hidden  black;
+        }
+
+        .seccion-larga {
+            background-color: #cb0000;
+            padding-top: 20px;
+            padding-right: 40px;
+            padding-bottom: 20px;
+            padding-left: 40px;
+            margin-top: 20px;
+            margin-right: 30px;
+            margin-bottom: 40px;
+            margin-left: 50px;
         }
 
     </style>
@@ -24,6 +43,23 @@ echo"Hola mundo!!!"
     <h5>Hola Cara de bola </h5>
     <h6>Hola Cara de bola </h6>
 
-    
+    <div class="seccion">
+        <h2>Título del párrafo</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit e
+        sse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+        culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+    <div class="seccion-larga">
+        <h2>Título del párrafo</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit e
+        sse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+        culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
  </body>
  </html>
